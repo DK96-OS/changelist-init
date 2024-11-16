a26dc44d12a98066c0f807c67773802a518ab259
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-11-15 19:02:09 -0500

    Release 0.1.2 (#6)
    Changelist Init:
    * Update changelist_init/__init__.py - provide include_untracked argument
    * Update changelist_init/git/__init__.py - modify method get_status_lists (add parameter include_untracked)
    * Update changelist_init/git/status_reader.py - skip lines ending with a slash (because they are dirs)
    * Update changelist_init/git/status_runner.py - add method run_untracked_status, add no-optional-locks argument, debug argument, disable renames, fix lint
    
    Github:
    * Create merge_test.yml
    * Update merge_test.yml - disable fail-fast, ensure git is installed
    
    Project Root:
    * Update setup.py - increase version
    
    Pull Requests:
    * Debug: IncludeUntracked missing files in new untracked directories  (#7)
    
    Test:
    * Update test/changelist_init/git/test_status_runner.py - add test cases, add test fixture, fix test case name
    * Update test/changelist_init/test_init.py - alter test case expectations

--- changelist_init/git/__init__.py
+++ changelist_init/git/__init__.py
@@ -4,12 +4,14 @@ from changelist_init.git.git_status_lists import GitStatusLists
 from changelist_init.git import status_runner, status_reader
 
 
-def get_status_lists() -> GitStatusLists:
+def get_status_lists(
+    include_untracked: bool = False,
+) -> GitStatusLists:
     """ Executes the Complete Git Status into File Change Operation.
 
     Returns:
     list[FileChange] - The List of FileChange information from Git Status.
     """
     return status_reader.read_git_status_output(
-        status_runner.run_git_status()
+        status_runner.run_git_status() if not include_untracked else status_runner.run_untracked_status()
     )
