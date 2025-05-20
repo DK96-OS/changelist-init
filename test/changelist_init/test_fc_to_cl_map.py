b154b7e27011455634d4ccf12b61105e0697fbc2
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2025-05-19 21:55:14 -0400

    Test Main Package: * Create test_fc_to_cl_map

---
+++ test/changelist_init/test_fc_to_cl_map.py
@@ -0,0 +1,29 @@
+""" Testing FC To CL Map Module Methods.
+"""
+from changelist_data.file_change import FileChange
+
+from changelist_init.fc_to_cl_map import create_fc_to_cl_dict, offer_fc_to_cl_dict, merge_fc_generator
+from test.changelist_init.conftest import get_sample_fc_path, create_fc
+
+
+def test_create_fc_to_cl_dict_empty_list_returns_empty_dict():
+    result = create_fc_to_cl_dict([])
+    assert len(result.keys()) == 0
+
+
+def test_offer_fc_into_cl_dict_scenario_0():
+    assert not offer_fc_to_cl_dict({}, create_fc(get_sample_fc_path(0)))
+
+
+def test_offer_fc_into_cl_dict_scenario_0_empty_fc_raises_exit():
+    try:
+        assert not offer_fc_to_cl_dict({}, FileChange())
+        raised_exit = False
+    except SystemExit:
+        raised_exit = True
+    assert raised_exit
+
+
+def test_merge_fc_generator_scenario_0():
+    result = list(merge_fc_generator([], []))
+    assert len(result) == 0
