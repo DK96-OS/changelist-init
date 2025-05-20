97c41923b9064567a34dbda4689943cb7d287955
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2025-05-20 16:41:30 -0400

    Test Main Package: * Update conftest - remove test fixtures replaced by FileChange constructors * Update test_fc_to_cl_map - implement FileChange constructors

--- test/changelist_init/test_fc_to_cl_map.py
+++ test/changelist_init/test_fc_to_cl_map.py
@@ -1,9 +1,9 @@
 """ Testing FC To CL Map Module Methods.
 """
-from changelist_data.file_change import FileChange
+from changelist_data import file_change
 
 from changelist_init.fc_to_cl_map import create_fc_to_cl_dict, offer_fc_to_cl_dict, merge_fc_generator
-from test.changelist_init.conftest import get_sample_fc_path, create_fc
+from test.changelist_init.conftest import get_sample_fc_path
 
 
 def test_create_fc_to_cl_dict_empty_list_returns_empty_dict():
@@ -12,12 +12,12 @@ def test_create_fc_to_cl_dict_empty_list_returns_empty_dict():
 
 
 def test_offer_fc_into_cl_dict_scenario_0():
-    assert not offer_fc_to_cl_dict({}, create_fc(get_sample_fc_path(0)))
+    assert not offer_fc_to_cl_dict({}, file_change.create_fc(get_sample_fc_path(0)))
 
 
 def test_offer_fc_into_cl_dict_scenario_0_empty_fc_raises_exit():
     try:
-        assert not offer_fc_to_cl_dict({}, FileChange())
+        assert not offer_fc_to_cl_dict({}, file_change.FileChange())
         raised_exit = False
     except SystemExit:
         raised_exit = True
