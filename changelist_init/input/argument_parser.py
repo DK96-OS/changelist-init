ad1f845529e74eeeb415a77efac8c384f163dab1
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2025-05-20 16:49:19 -0400

    CL-Init Input Package: * Update __init__ - add docs, add verbose parameter to validate_input method to control printing confirmation after sort.xml is created * Update argument_data - update docs format * Update argument_parser - update docs format, add generate_sort_xml and sort_xml arguments * Update string_validation - update docs

--- changelist_init/input/argument_parser.py
+++ changelist_init/input/argument_parser.py
@@ -1,8 +1,6 @@
-"""Defines and Validates Argument Syntax.
-
-Encapsulates Argument Parser.
-
-Returns Argument Data, the args provided by the User.
+""" Defines and Validates Argument Syntax.
+ - Encapsulates Argument Parser.
+ - Returns Argument Data, the args provided by the User.
 """
 from argparse import ArgumentParser
 from sys import exit
@@ -15,14 +13,13 @@ from changelist_init.input.string_validation import validate_name
 def parse_arguments(
     args: Optional[list[str]] = None
 ) -> ArgumentData:
-    """
-    Parse command line arguments.
+    """ Parse command line arguments.
 
-    Parameters:
-    - args: A list of argument strings.
+**Parameters:**
+ - args: A list of argument strings.
 
-    Returns:
-    ArgumentData : Container for Valid Argument Data.
+**Returns:**
+ ArgumentData : Container for Valid Argument Data.
     """
     if args is None or len(args) == 0:
         return ArgumentData()
@@ -37,14 +34,14 @@ def parse_arguments(
 def _validate_arguments(
     parsed_args,
 ) -> ArgumentData:
-    """Checks the values received from the ArgParser.
-        Uses Validate Name method from StringValidation.
+    """ Checks the values received from the ArgParser.
+- Uses Validate Name method from StringValidation.
 
-    Parameters:
-    - parsed_args : The object returned by ArgumentParser.
+**Parameters:**
+ - parsed_args : The object returned by ArgumentParser.
 
-    Returns:
-    ArgumentData - A DataClass of syntactically correct arguments.
+**Returns:**
+ ArgumentData - A DataClass of syntactically correct arguments.
     """
     if (changelists_file := parsed_args.changelists_file) is not None:
         if not validate_name(changelists_file):
@@ -61,16 +58,16 @@ def _validate_arguments(
 
 
 def _define_arguments() -> ArgumentParser:
-    """
-    Initializes and Defines Argument Parser.
-       - Sets Required/Optional Arguments and Flags.
+    """ Initializes and Defines Argument Parser.
+ - Sets Required/Optional Arguments and Flags.
 
-    Returns:
-    argparse.ArgumentParser - An instance with all supported Arguments.
+**Returns:**
+ argparse.ArgumentParser - An instance with all supported Arguments.
     """
     parser = ArgumentParser(
         description="Initializes and updates the Changelist data storage file with git status information.",
     )
+    parser.color = True
     # Optional Arguments
     parser.add_argument(
         '--changelists_file',
@@ -89,5 +86,11 @@ def _define_arguments() -> ArgumentParser:
         action='store_true',
         default=False,
         help='The option to include untracked files in changelists.',
+    )
+    parser.add_argument(
+        "--generate_sort_xml", "--sort_xml",
+        action='store_true',
+        default=False,
+        help='Generate the .changelist/sort.xml file for the project.',
     )
     return parser
