051c25b5d8fbc7c17e218f7a938d34ea0c99d9dd
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-09-01 15:23:04 +0200

    Root Project Python Files: * Update setup - increase changelist-data min version 0.2.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
 	license='GPLv3',
     packages=find_packages(exclude=['test', 'test.*']),
     install_requires=[
-        'changelist-data >= 0.2.3, < 0.3',
+        'changelist-data >= 0.2.4, < 0.3',
     ],
     entry_points={
         'console_scripts': [
