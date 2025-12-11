44ec602e3360b0d1c52b5d2e69b41896a8371674
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-12-11 10:39:24 -0500

    Test Input Package: * Update test_method_validate_input - use temp_cwd fixture, implement pytest.raises assertions * Remove test_string_validation

--- test/changelist_init/input/test_method_validate_input.py
+++ test/changelist_init/input/test_method_validate_input.py
@@ -1,45 +1,35 @@
 """ Testing Input Init Package Method.
 """
+import pytest
+
 from changelist_init.input import validate_input
 
 
-def test_validate_input_():
+def test_validate_input_(temp_cwd):
     args = []
     result = validate_input(args)
     assert len(result.storage.get_changelists()) == 0
 
 
-def test_validate_input_include_untracked_():
+def test_validate_input_include_untracked_(temp_cwd):
     args = ['--include_untracked']
     result = validate_input(args)
     assert len(result.storage.get_changelists()) == 0
 
 
-def test_validate_input_invalid_changelists_raises_exit():
+def test_validate_input_invalid_changelists_raises_exit(temp_cwd):
     args = ['--changelists_file', '  ']
-    try:
+    with pytest.raises(SystemExit):
         validate_input(args)
-        raises_exit = False
-    except SystemExit:
-        raises_exit = True
-    assert raises_exit
 
 
-def test_validate_input_invalid_workspace_raises_exit():
+def test_validate_input_invalid_workspace_raises_exit(temp_cwd):
     args = ['--workspace_file', '  ']
-    try:
+    with pytest.raises(SystemExit):
         validate_input(args)
-        raises_exit = False
-    except SystemExit:
-        raises_exit = True
-    assert raises_exit
 
 
-def test_validate_input_invalid_arg_raises_exit():
+def test_validate_input_invalid_arg_raises_exit(temp_cwd):
     args = ['--unknown_arg']
-    try:
+    with pytest.raises(SystemExit):
         validate_input(args)
-        raises_exit = False
-    except SystemExit:
-        raises_exit = True
-    assert raises_exit
