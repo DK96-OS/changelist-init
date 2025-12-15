04afebffeb5f811f50bf5b03d4e7e18d40827f8a
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-12-15 10:34:33 -0500

    CL-Init Git Package: * Update git_status_lists - update docs, reorder GitStatusLists fields, update method parameter names

--- changelist_init/git/git_status_lists.py
+++ changelist_init/git/git_status_lists.py
@@ -1,19 +1,15 @@
 """ Container for the Git Status Information.
-    Files in Git Status are categorized into 3 groups.
-    1. Staged
-    2. Unstaged
-    3. Untracked
-    These three groups are stored in three lists in this data class.
+ - Files are categorized into 4 lists.
 
 **GitFileStatus NamedTuple Fields:**
  - code (str): The two character code describing the git file status.
  - file_path (str): The String path of the file, relative to repo directory.
 
 **GitStatusLists NamedTuple Fields:**
- - untracked (list):
- - unstaged (list):
- - staged (list):
- - partial_stage (list):
+ - staged (list): The list of staged GitFileStatus tuples.
+ - unstaged (list): The list of unstaged GitFileStatus tuples.
+ - partial_stage (list): The list of partially staged GitFileStatus tuples. Some changes are staged, some not.
+ - untracked (list): The list of untracked GitFileStatus tuples.
 """
 from collections import namedtuple
 from typing import Generator
@@ -29,45 +25,68 @@ GitFileStatus = namedtuple(
 
 GitStatusLists = namedtuple(
     'GitStatusLists',
-    'untracked unstaged staged partial_stage',
+    'staged unstaged partial_stage untracked',
 )
 
 
 def get_list(
-    self: GitStatusLists,
+    status_lists: GitStatusLists,
     tracking_status: GitTrackingStatus,
 ) -> list[GitFileStatus]:
     """ Obtain a List of File Status objects with the given Tracking Status.
+
+**Parameters:**
+ - status_lists (GitStatusLists): The collections of GitFileStatus objects.
+ - tracking_status (GitTrackingStatus): The status group to obtain the list for.
+
+**Returns:**
+ list[GitFileStatus] - The specified list of GitFileStatus objects.
     """
     match tracking_status:
         case GitTrackingStatus.UNSTAGED:
-            return self.unstaged
+            return status_lists.unstaged
         case GitTrackingStatus.STAGED:
-            return self.staged
+            return status_lists.staged
         case GitTrackingStatus.PARTIAL_STAGE:
-            return self.partial_stage
+            return status_lists.partial_stage
     # Default: GitTrackingStatus.UNTRACKED
-    return self.untracked
+    return status_lists.untracked
 
 
 def add_file_status(
-    self: GitStatusLists,
+    status_lists: GitStatusLists,
     file_status: GitFileStatus,
 ) -> bool:
-    """ Add A File Status to the Lists.
+    """ Add a File Status to the Lists.
+
+**Parameters:**
+ - status_lists (GitStatusLists): The collections of GitFileStatus objects.
+ - file_status (GitFileStatus): The object containing status code and file.
+
+**Returns:**
+ bool - False if the file_status code was not able to be decoded.
     """
-    get_list(self, from_str(file_status.code)).append(file_status)
+    if (tracking_status := from_str(file_status.code)) is None:
+        return False
+    get_list(status_lists, tracking_status).append(file_status)
     return True
 
 
 def merge_all(
-    self: GitStatusLists,
+    status_lists: GitStatusLists,
     include_untracked: bool = True,
 ) -> Generator[GitFileStatus, None, None]:
     """ Combine all List contents into the Generator output, in this order: staged, partial, unstaged, untracked.
+
+**Parameters:**
+ - status_lists (GitStatusLists): The collections of GitFileStatus objects.
+ - include_untracked (bool): Whether to include the untracked GitFileStatus objects in the generated output.
+
+**Yields:**
+ GitFileStatus - The GitFileStatus objects from the status_lists collection.
     """
-    yield from self.staged
-    yield from self.partial_stage
-    yield from self.unstaged
+    yield from status_lists.staged
+    yield from status_lists.partial_stage
+    yield from status_lists.unstaged
     if include_untracked:
-        yield from self.untracked
+        yield from status_lists.untracked
