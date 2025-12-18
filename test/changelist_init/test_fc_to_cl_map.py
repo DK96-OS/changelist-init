0b7249aa16cb8856402b37f6dddd7d32f29eaf56
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-12-18 01:29:23 -0500

    Test Main Package: * Update test_fc_to_cl_map - add test cases

--- test/changelist_init/test_fc_to_cl_map.py
+++ test/changelist_init/test_fc_to_cl_map.py
@@ -1,9 +1,11 @@
 """ Testing FC To CL Map Module Methods.
 """
-from changelist_data import file_change
+import pytest
+from changelist_data import file_change, Changelist
+from changelist_data.file_change import create_fc
 
 from changelist_init.fc_to_cl_map import create_fc_to_cl_dict, offer_fc_to_cl_dict, merge_fc_generator
-from test.changelist_init.conftest import get_sample_fc_path
+from test.changelist_init.conftest import get_sample_fc_path, cl_sample_list, _SAMPLE_FC_0
 
 
 def test_create_fc_to_cl_dict_empty_list_returns_empty_dict():
@@ -11,17 +13,53 @@ def test_create_fc_to_cl_dict_empty_list_returns_empty_dict():
     assert len(result.keys()) == 0
 
 
-def test_offer_fc_into_cl_dict_scenario_0():
+def test_create_fc_to_cl_dict_():
+    result = create_fc_to_cl_dict(cl_sample_list(['c', ' ', ' ']))
+    assert len(result.keys()) == 1
+
+
+@pytest.mark.parametrize(
+    "test_cl_sample_list, expected_count", [
+        (cl_sample_list(['c', '', '']), 1),
+        (cl_sample_list(['u', ' d', '']), 2),
+        (cl_sample_list(['cud', '', '']), 3),
+        (cl_sample_list(['c', 'c', 'c']), 1),
+        (cl_sample_list(['cc', 'cc', 'cc']), 2),
+    ]
+)
+def test_create_fc_to_cl_dict_cl_sample_lists(
+    test_cl_sample_list, expected_count
+):
+    result = create_fc_to_cl_dict(test_cl_sample_list)
+    assert len(result.keys()) == expected_count
+
+
+def test_offer_fc_to_cl_dict_scenario_0():
     assert not offer_fc_to_cl_dict({}, file_change.create_fc(get_sample_fc_path(0)))
 
 
-def test_offer_fc_into_cl_dict_scenario_0_empty_fc_raises_exit():
-    try:
-        assert not offer_fc_to_cl_dict({}, file_change.FileChange())
-        raised_exit = False
-    except SystemExit:
-        raised_exit = True
-    assert raised_exit
+def test_offer_fc_to_cl_dict__empty_fc_raises_exit():
+    with pytest.raises(SystemExit):
+        offer_fc_to_cl_dict({}, file_change.FileChange())
+
+
+def test_offer_fc_to_cl_dict_create_sample_fc_0():
+    test_map: dict[str, Changelist] = create_fc_to_cl_dict(
+        cl_sample_list(['c', '', ''])
+    )
+    assert offer_fc_to_cl_dict(test_map, create_fc(_SAMPLE_FC_0))
+    assert _SAMPLE_FC_0 in test_map
+    default_cl: Changelist = test_map[_SAMPLE_FC_0]
+    assert default_cl.is_default
+
+
+def test_offer_fc_to_cl_dict_create_sample_fc_3():
+    test_map = create_fc_to_cl_dict(cl_sample_list(['c', ' c', '  c']))
+    for i in range(3):
+        assert offer_fc_to_cl_dict(test_map, create_fc(get_sample_fc_path(i)))
+        assert get_sample_fc_path(i) in test_map
+        cl: Changelist = test_map[get_sample_fc_path(i)]
+        assert len(cl.changes) == 1
 
 
 def test_merge_fc_generator_scenario_0():
