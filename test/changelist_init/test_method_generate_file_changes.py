3f226c5daddca87d46db9c7cbde20918d1605a58
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-12-18 01:33:39 -0500

    Test Main Package: * Rename test_method_initialize_file_changes to test_method_generate_file_changes - refactor method test cases

--- test/changelist_init/test_method_generate_file_changes.py
+++ test/changelist_init/test_method_generate_file_changes.py
@@ -1,11 +1,11 @@
-""" Testing changelist_init Package-Level Method initialize_file_changes.
+""" Testing changelist_init Package-Level Method generate_file_changes.
 """
 import subprocess
 from typing import Callable
 
 import pytest
 
-from changelist_init import initialize_file_changes
+from changelist_init.git import generate_file_changes
 
 from test.changelist_init.conftest import FC_PATH_SETUP
 
@@ -24,140 +24,129 @@ def mock_subprocess(output: str) -> Callable:
     return lambda **kwargs: wrap_stdout(output)
 
 
-def test_initialize_file_changes_tracked_only_given_single_untracked_returns_empty_list(
-    input_tracked,
+def test_generate_file_changes_tracked_only_given_single_untracked_returns_empty_list(
     git_status_line_untracked_setup
 ):
     with pytest.MonkeyPatch.context() as c:
         c.setattr(subprocess, 'run', mock_subprocess(git_status_line_untracked_setup))
-        result = initialize_file_changes(input_tracked)
+        result = list(generate_file_changes(include_untracked=False))
     assert len(result) == 0
 
 
-def test_initialize_file_changes_all_changes_given_single_untracked_returns_file_change(
+def test_generate_file_changes_all_changes_given_single_untracked_returns_file_change(
     input_all,
     git_status_line_untracked_setup
 ):
     with pytest.MonkeyPatch.context() as c:
         c.setattr(subprocess, 'run', mock_subprocess(git_status_line_untracked_setup))
-        result = initialize_file_changes(input_all)
+        result = list(generate_file_changes(input_all.include_untracked))
     assert len(result) == 1
     assert result[0].before_path is None
     assert result[0].after_path == FC_PATH_SETUP
 
 
-def test_initialize_file_changes_tracked_only_given_single_unstaged_create_returns_file_change(
-    input_tracked,
+def test_generate_file_changes_tracked_only_given_single_unstaged_create_returns_file_change(
     git_status_line_unstaged_create_setup
 ):
     with pytest.MonkeyPatch.context() as c:
         c.setattr(subprocess, 'run', mock_subprocess(git_status_line_unstaged_create_setup))
-        result = initialize_file_changes(input_tracked)
+        result = list(generate_file_changes(False))
     assert len(result) == 1
     assert result[0].before_path is None
     assert result[0].after_path == FC_PATH_SETUP
 
 
-def test_initialize_file_changes_all_changes_given_single_unstaged_create_returns_file_change(
-    input_all,
+def test_generate_file_changes_all_changes_given_single_unstaged_create_returns_file_change(
     git_status_line_unstaged_create_setup
 ):
     with pytest.MonkeyPatch.context() as c:
         c.setattr(subprocess, 'run', mock_subprocess(git_status_line_unstaged_create_setup))
-        result = initialize_file_changes(input_all)
+        result = list(generate_file_changes(True))
     assert len(result) == 1
     assert result[0].before_path is None
     assert result[0].after_path == FC_PATH_SETUP
 
 
-def test_initialize_file_changes_tracked_only_given_single_staged_create_returns_file_change(
-    input_tracked,
+def test_generate_file_changes_tracked_only_given_single_staged_create_returns_file_change(
     git_status_line_staged_create_setup
 ):
     with pytest.MonkeyPatch.context() as c:
         c.setattr(subprocess, 'run', mock_subprocess(git_status_line_staged_create_setup))
-        result = initialize_file_changes(input_tracked)
+        result = list(generate_file_changes(False))
     assert len(result) == 1
     assert result[0].before_path is None
     assert result[0].after_path == FC_PATH_SETUP
 
 
-def test_initialize_file_changes_all_changes_given_single_staged_create_returns_file_change(
-    input_all,
+def test_generate_file_changes_all_changes_given_single_staged_create_returns_file_change(
     git_status_line_staged_create_setup
 ):
     with pytest.MonkeyPatch.context() as c:
         c.setattr(subprocess, 'run', mock_subprocess(git_status_line_staged_create_setup))
-        result = initialize_file_changes(input_all)
+        result = list(generate_file_changes(True))
     assert len(result) == 1
     assert result[0].before_path is None
     assert result[0].after_path == FC_PATH_SETUP
 
 
-def test_initialize_file_changes_tracked_only_given_single_unstaged_modify_returns_file_change(
-    input_tracked,
+def test_generate_file_changes_tracked_only_given_single_unstaged_modify_returns_file_change(
     git_status_line_unstaged_modify_setup
 ):
     with pytest.MonkeyPatch.context() as c:
         c.setattr(subprocess, 'run', mock_subprocess(git_status_line_unstaged_modify_setup))
-        result = initialize_file_changes(input_tracked)
+        result = list(generate_file_changes(False))
     assert len(result) == 1
     assert result[0].after_path == result[0].before_path
     assert result[0].after_path == FC_PATH_SETUP
 
 
-def test_initialize_file_changes_all_changes_given_single_unstaged_modify_returns_file_change(
-    input_all,
+def test_generate_file_changes_all_changes_given_single_unstaged_modify_returns_file_change(
     git_status_line_unstaged_modify_setup
 ):
     with pytest.MonkeyPatch.context() as c:
         c.setattr(subprocess, 'run', mock_subprocess(git_status_line_unstaged_modify_setup))
-        result = initialize_file_changes(input_all)
+        result = list(generate_file_changes(True))
     assert len(result) == 1
     assert result[0].after_path == result[0].before_path
     assert result[0].after_path == FC_PATH_SETUP
 
 
-def test_initialize_file_changes_tracked_only_given_single_staged_modify_returns_file_change(
-    input_tracked,
+def test_generate_file_changes_tracked_only_given_single_staged_modify_returns_file_change(
     git_status_line_staged_modify_setup
 ):
     with pytest.MonkeyPatch.context() as c:
         c.setattr(subprocess, 'run', mock_subprocess(git_status_line_staged_modify_setup))
-        result = initialize_file_changes(input_tracked)
+        result = list(generate_file_changes(False))
     assert len(result) == 1
     assert result[0].after_path == result[0].before_path
     assert result[0].after_path == FC_PATH_SETUP
 
 
-def test_initialize_file_changes_all_changes_given_single_staged_modify_returns_file_change(
-    input_all,
+def test_generate_file_changes_all_changes_given_single_staged_modify_returns_file_change(
     git_status_line_staged_modify_setup
 ):
     with pytest.MonkeyPatch.context() as c:
         c.setattr(subprocess, 'run', mock_subprocess(git_status_line_staged_modify_setup))
-        result = initialize_file_changes(input_all)
+        result = list(generate_file_changes(True))
     assert len(result) == 1
     assert result[0].after_path == result[0].before_path
     assert result[0].after_path == FC_PATH_SETUP
 
 
-def test_initialize_file_changes_tracked_only_given_multi_init_this_returns_file_changes(
-    input_tracked,
+def test_generate_file_changes_tracked_only_given_multi_init_this_returns_file_changes(
     git_status_line_multi_init_this
 ):
     with pytest.MonkeyPatch.context() as c:
         c.setattr(subprocess, 'run', mock_subprocess(git_status_line_multi_init_this))
-        result = initialize_file_changes(input_tracked)
+        result = list(generate_file_changes(False))
     assert len(result) == 32
 
 
-def test_initialize_file_changes_all_changes_given_multi_init_this_returns_file_changes(
-    input_all,
+def test_generate_file_changes_all_changes_given_multi_init_this_returns_file_changes(
     git_status_line_multi_init_this
 ):
     with pytest.MonkeyPatch.context() as c:
         c.setattr(subprocess, 'run', mock_subprocess(git_status_line_multi_init_this))
-        result = initialize_file_changes(input_all)
+        result = list(generate_file_changes(True))
     # Includes untracked files, but ignores Directories
     assert len(result) == 33
