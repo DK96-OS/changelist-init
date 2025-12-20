261d7a0b3de586761735588045acf7bcb18f83df
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-12-20 09:55:00 -0500

    CL-Init Git Package: * Update status_change_mapping - update docs, modify method map_file_status_to_changes (print unrecognized codes instead of exiting), modify method get_status_code_change_map (return None instead of exit), hide method map_status_path_to_change

--- changelist_init/git/status_change_mapping.py
+++ changelist_init/git/status_change_mapping.py
@@ -14,30 +14,32 @@ def map_file_status_to_changes(
 ) -> Generator[FileChange, None, None]:
     """ Categorize by Status Code, and Map to FileChange data objects.
 
-    Parameters:
-    - git_files (Iterable[GitFileStatus]): An iterable or Generator providing GitFileStatus objects.
+**Parameters:**
+ - git_files (Iterable[GitFileStatus]): An iterable or Generator providing GitFileStatus objects.
 
-    Returns:
-    FileChange - Yield by Generator.
+**Returns:**
+ FileChange - Yield by Generator.
     """
     for code, group in groupby(git_files, lambda w: w.code):
-        mapping_function = get_status_code_change_map(code)
+        if (mapping_function := get_status_code_change_map(code)) is None:
+            print(f"Unrecognized Git Status Code:({code})")
+            continue
         for file_status in group:
             yield mapping_function(
-                map_status_path_to_change(file_status.file_path)
+                _map_status_path_to_change(file_status.file_path)
             )
 
 
 def get_status_code_change_map(
     code: str,
-) -> Callable[[str, ], FileChange]:
+) -> Callable[[str, ], FileChange] | None:
     """ Get a FileChange mapping callable for a specific code.
 
-    Parameters:
-    - code (str): The status code, determining what kind of FileChange (create, modify, delete)
+**Parameters:**
+ - code (str): The status code, determining what kind of FileChange (create, modify, delete)
 
-    Returns:
-    Callable[str, FileChange] - A function that maps a FileChange path into the FileChange object.
+**Returns:**
+ Callable[str, FileChange]? - A function that maps a FileChange path into the FileChange object. None if the code is unrecognized.
     """
     if code in ('M ', ' M', 'MM'):
         return file_change.update_fc
@@ -47,13 +49,13 @@ def get_status_code_change_map(
         return file_change.delete_fc
     if '?' in code or '!' in code:
         return file_change.create_fc
-    exit(f"Unknown Code: {code}")
+    return None
 
 
-def map_status_path_to_change(
+def _map_status_path_to_change(
     status_path: str,
 ) -> str:
     """ Convert Status File path to FileChange path.
-        Adds a leading slash character.
+ - Adds a leading slash character if not present.
     """
     return '/' + status_path if not status_path.startswith('/') else status_path
