177bbdff0e62565993626a019c419ecbb17e83d4
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-12-20 16:48:00 -0500

    Test Git Package: * Create test_git_tracking_status

---
+++ test/changelist_init/git/test_status_change_mapping.py
@@ -0,0 +1,52 @@
+""" Testing Status Change Mapping Method.
+"""
+import pytest
+from changelist_data import file_change
+
+from changelist_init.git.status_change_mapping import get_status_code_change_map
+
+
+@pytest.mark.parametrize(
+    'status_code', [
+        'A ',
+        ' A',
+        'AM',
+        'MA',
+    ]
+)
+def test_get_status_code_change_map_create_inputs_returns_create_fc(status_code):
+    assert file_change.create_fc == get_status_code_change_map(status_code)
+
+
+@pytest.mark.parametrize(
+    'status_code', [
+        'M ',
+        ' M',
+        'MM',
+        'MM',
+    ]
+)
+def test_get_status_code_change_map_update_inputs_returns_update_fc(status_code):
+    assert file_change.update_fc == get_status_code_change_map(status_code)
+
+
+@pytest.mark.parametrize(
+    'status_code', [
+        'D ',
+        ' D',
+        'DM',
+        'MD',
+    ]
+)
+def test_get_status_code_change_map_delete_inputs_returns_delete_fc(status_code):
+    assert file_change.delete_fc == get_status_code_change_map(status_code)
+
+
+@pytest.mark.parametrize(
+    'status_code', [
+        '??',
+        '!!',
+    ]
+)
+def test_get_status_code_change_map_untracked_inputs_returns_create_fc(status_code):
+    assert file_change.create_fc == get_status_code_change_map(status_code)
